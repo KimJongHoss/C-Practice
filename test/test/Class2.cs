52685f088ec4d0d5c8b799e4028d96413d1f289c
KimJongHoss <haberde97@gmail.com>
2024-07-04 13:52:21 +0900

    upload sources

---
+++ test/test/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using jongho;
+using CondingOn;
+
+namespace test
+{
+    internal class Class2
+    {
+
+        CondingOn.School school;
+        jongho.School school2;
+
+        //School sc = new School();
+
+        
+
+    }
+}
---
+++ test/test/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // MessageBox.Show("Hello world");
+
+            //textBox1_print.Text = "이것은\r\n멀티라인\r\n입니다";
+
+            //{ 
+            //    int a = 10;
+            //    textBox1_print.Text = a.ToString();
+            //}
+
+            //string s = "abcdef";
+            //int c = s.IndexOf("b");
+            //textBox1_print.Text = c.ToString();
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int x = 0;
+            foreach (int number in numbers)
+            {
+                x += number;
+            }
+
+            textBox1_print.Text = x.ToString();
+
+
+        }
+    }
+}
