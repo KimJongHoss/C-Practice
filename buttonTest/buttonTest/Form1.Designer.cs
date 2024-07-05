9f40fef27829d7181078093f42d2366a51b8115f
KimJongHoss <haberde97@gmail.com>
2024-07-05 13:47:48 +0900

    complete buttonTest

---
+++ buttonTest/buttonTest/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace buttonTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.hello = new System.Windows.Forms.Button();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.textBox_copy = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(359, 304);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // hello
+            // 
+            this.hello.Location = new System.Drawing.Point(109, 343);
+            this.hello.Name = "hello";
+            this.hello.Size = new System.Drawing.Size(262, 95);
+            this.hello.TabIndex = 1;
+            this.hello.Text = "Input";
+            this.hello.UseVisualStyleBackColor = true;
+            this.hello.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(414, 12);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(374, 304);
+            this.textBox_input.TabIndex = 2;
+            // 
+            // textBox_copy
+            // 
+            this.textBox_copy.Location = new System.Drawing.Point(414, 343);
+            this.textBox_copy.Multiline = true;
+            this.textBox_copy.Name = "textBox_copy";
+            this.textBox_copy.Size = new System.Drawing.Size(374, 95);
+            this.textBox_copy.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_copy);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.hello);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button hello;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.TextBox textBox_copy;
+    }
+}
+
---
+++ buttonTest/buttonTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buttonTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            textBox_input.Text += textBox1.Text.ToString()+"\r\n";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox_copy.Text = textBox1.Text.ToString()+"\r\n";
+        }
+    }
+}
